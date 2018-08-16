c14f23d23a7beaeda4992253c127da426b24c5b5
Graithen <grey.jim@gmail.com>
2018-08-16 18:51:21 +0100

    Started implementation of complex wave movements

---
+++ Project Files/Epitaph/Assets/Scripts/Enemy Controllers/ControlledStandardEnemyController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlledStandardEnemyController : MonoBehaviour {
+
+	public int health = 100;
+
+	//Movement Variables
+	public float movementSpeed = 3;
+
+	//Attack Variables
+	public float delayBetweenProjectiles = 1.5f;
+	public GameObject frontEmitter;
+	public GameObject projectile1;
+	public GameObject projectile2;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Attack"); //starts a coroutine running for firing projectiles
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+		Status ();
+	}
+
+
+	void Movement () {
+		
+	}
+
+	void Status () {
+		if (health <= 0) {
+			Destroy (gameObject);
+			GameController.gameScore += 10;
+		}
+	}
+
+	IEnumerator Attack () {
+		while (true) {
+			Instantiate (projectile1, frontEmitter.transform.position, Quaternion.identity);
+			yield return new WaitForSeconds (delayBetweenProjectiles);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "PlayerProjectile") {
+			health -= 50;
+			Destroy (other.gameObject);
+		}
+	}
+
+	void OnBecameInvisible ()
+	{
+		Destroy (gameObject);
+	}
+}
---
+++ Project Files/Epitaph/Assets/StandardEnemyWaveController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardEnemyWaveController : MonoBehaviour {
+
+	//Arrays
+	public GameObject[] enemiesInWave;
+	public GameObject[] navigationPoints;
+	public int navPointsComplete = 0;
+
+	public Vector2 firstEnemyLocation;
+	public Vector2 secondEnemyLocation;
+	public Vector2 thirdEnemyLocation;
+
+	//Movement
+	public float waveSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+
+	void Movement () {
+		
+		firstEnemyLocation = enemiesInWave [0].transform.localPosition;
+		secondEnemyLocation = enemiesInWave [1].transform.localPosition;
+		thirdEnemyLocation = enemiesInWave [2].transform.localPosition;
+
+	}
+
+}
