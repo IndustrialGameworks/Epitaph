362892624943436c1457081460abbe7bac7e0abb
Cristiwan <crisgalliano1998@gmail.com>
2018-08-18 18:31:46 +0100

    fixed lotus and burst positioning and added burst point indicator

---
+++ Project Files/Epitaph/Assets/Scripts/Enemy Controllers/BurstParent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurstParent : MonoBehaviour {
+
+	public BurstEnemyController BurstController;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine ("destroyThis");
+	}
+
+	IEnumerator destroyThis ()
+	{
+		if (BurstController.isDestroyed == true) 
+		{
+			yield return new WaitForSeconds (0.5f);
+			Destroy (gameObject);
+		}
+	}
+
+	void OnBecameInvisible ()
+	{
+		Destroy (gameObject);
+	}
+}
