e622de24a18256519e076d74433f8a40402ceeed
Cristiwan <crisgalliano1998@gmail.com>
2018-08-18 22:49:21 +0100

    0.0.2.1

--- Project Files/Epitaph/Assets/Scripts/Game Scripts/PlayScript.cs
+++ Project Files/Epitaph/Assets/Scripts/Game Scripts/PlayScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-[assembly: System.Reflection.AssemblyVersion("0.0.2.0")]
+[assembly: System.Reflection.AssemblyVersion("0.0.2.1")]
 
 public class PlayScript : MonoBehaviour 
 {
