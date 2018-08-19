04088ec6428d19fbe614d89971f65a93fdd51e94
Graithen <grey.jim@gmail.com>
2018-08-19 21:07:58 +0100

    Added primer boss, initial functionality
    Assets and initial functionality implemented. Primer turrets track player but are  not yet prefab ready.

---
+++ Project Files/Epitaph/Assets/Scripts/Enemy Controllers/Bosses/Primer/PrimerBossController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimerBossController : MonoBehaviour {
+
+	//Arrays
+	public GameObject [] turrets;
+
+	//Movement
+	public float movementSpeed = 2f;
+	bool constructLocation; //if true, new target location will be constructed;
+	float minConstraintX = 5;
+	float maxConstraintX = 8;
+	float minConstraintY = 1;
+	float maxConstraintY = -1;
+	public float currentLocationX; //current location X
+	public float currentLocationY; //current location Y
+	float randomX;
+	float randomY;
+	Vector2 randomVector;
+
+	//Turret attack
+
+	//Core door control
+	public bool coreDoorOpen; //if true, door opens
+	public GameObject coreDoor; //get the door object
+	public float coreDoorRotationSpeed = 5; //door rotation speed
+	float coreDoorRotationValue; //current door rotation
+
+	// Use this for initialization
+	void Start () {
+		RandomGeneration ();
+		StartCoroutine ("BuildRandomLocation");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CoreDoorMovement ();
+		Movement ();
+
+		if (constructLocation == true) {
+			RandomGeneration ();
+			constructLocation = false;
+		}
+	}
+
+	void Movement () {
+		transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), randomVector, movementSpeed * Time.deltaTime);
+		MovementChecks ();
+	}
+	
+	void RandomGeneration () { //generates random x and y coords within bounds
+		randomX = Random.Range (minConstraintX, maxConstraintX); //x constraints
+		randomY = Random.Range (minConstraintY, maxConstraintY); //y constraints
+		randomVector = new Vector2 (randomX, randomY); //constructs new random vector2
+		}
+
+	void CoreDoorMovement () {
+		coreDoorRotationValue = coreDoor.transform.rotation.z;
+		if (coreDoorOpen == true) {
+			if (coreDoorRotationValue < 0.999) {
+				coreDoor.transform.Rotate (0, 0, coreDoorRotationSpeed * Time.deltaTime);
+			} 
+			else {
+				//do nothing
+			}
+		}
+		if (coreDoorOpen == false) {
+			if (coreDoorRotationValue > 0) {
+				coreDoor.transform.Rotate (0, 0, -coreDoorRotationSpeed * Time.deltaTime);
+			} 
+			else {
+				//do nothing ,door rotation = 0
+			}
+		}
+	}
+
+	void MovementChecks () {
+		currentLocationX = transform.position.x;
+		currentLocationY = transform.position.y;
+	}
+
+	IEnumerator BuildRandomLocation () { //waits a set amount of time, the signals to construct a new new target vector
+		while (true) {
+			yield return new WaitForSeconds (5);
+			constructLocation = true;
+		}
+	}
+}
---
+++ Project Files/Epitaph/Assets/Scripts/Enemy Controllers/Bosses/Primer/PrimerTurret.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimerTurret : MonoBehaviour {
+
+	public int health = 1000;
+
+	public GameObject emitter;
+	public GameObject origin;
+	public GameObject player;
+	public float rotationSpeed = 5;
+	public float volleySeperation = 0.3f;
+	public float delayBetweenProjectiles = 4;
+
+	GameObject spawnedProjectile;
+	public Vector2 centerOfMass;
+
+	public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Attack");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player = GameObject.FindGameObjectWithTag ("Player"); //has to be be called on update during testing, as there is no player at the time this instance starts
+		RotateToTarget();
+		centerOfMass = origin.transform.position;
+
+
+	}
+
+	void RotateToTarget () { //code to rotate to look at target
+		Vector3 vectorToTarget = player.transform.position - transform.position;
+		float angle = Mathf.Atan2 (-vectorToTarget.y, -vectorToTarget.x) * Mathf.Rad2Deg; //take out the minuses if you want to invert the side that tracks the player
+		Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp (transform.rotation, q, Time.deltaTime * rotationSpeed);
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "PlayerProjectile") {
+			health -= 50;
+			Destroy (other.gameObject);
+		}
+	}
+
+	IEnumerator Attack () {
+		while (true) {
+			spawnedProjectile = Instantiate (projectile, emitter.transform.position, Quaternion.identity) as GameObject; //spawns projectile, returns gameObject
+			spawnedProjectile.gameObject.GetComponent<primerProjectile>().spawnOrigin = gameObject; //assigns this enemy instance to the projectile as point of origin
+
+			yield return new WaitForSeconds (volleySeperation);
+
+			spawnedProjectile = Instantiate (projectile, emitter.transform.position, Quaternion.identity) as GameObject; //spawns projectile, returns gameObject
+			spawnedProjectile.gameObject.GetComponent<primerProjectile>().spawnOrigin = gameObject; //assigns this enemy instance to the projectile as point of origin
+
+			yield return new WaitForSeconds (volleySeperation);
+
+			spawnedProjectile = Instantiate (projectile, emitter.transform.position, Quaternion.identity) as GameObject; //spawns projectile, returns gameObject
+			spawnedProjectile.gameObject.GetComponent<primerProjectile>().spawnOrigin = gameObject; //assigns this enemy instance to the projectile as point of origin
+
+			yield return new WaitForSeconds (volleySeperation);
+
+			spawnedProjectile = Instantiate (projectile, emitter.transform.position, Quaternion.identity) as GameObject; //spawns projectile, returns gameObject
+			spawnedProjectile.gameObject.GetComponent<primerProjectile>().spawnOrigin = gameObject; //assigns this enemy instance to the projectile as point of origin
+
+			yield return new WaitForSeconds (volleySeperation);
+
+			spawnedProjectile = Instantiate (projectile, emitter.transform.position, Quaternion.identity) as GameObject; //spawns projectile, returns gameObject
+			spawnedProjectile.gameObject.GetComponent<primerProjectile>().spawnOrigin = gameObject; //assigns this enemy instance to the projectile as point of origin
+
+			yield return new WaitForSeconds (volleySeperation);
+
+			spawnedProjectile = Instantiate (projectile, emitter.transform.position, Quaternion.identity) as GameObject; //spawns projectile, returns gameObject
+			spawnedProjectile.gameObject.GetComponent<primerProjectile>().spawnOrigin = gameObject; //assigns this enemy instance to the projectile as point of origin
+
+			yield return new WaitForSeconds (delayBetweenProjectiles);
+		}
+	}
+
+}
