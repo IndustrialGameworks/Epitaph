75fc78ad23027ed1f7e70efdbfbab7f47e6de174
Cristiwan <crisgalliano1998@gmail.com>
2018-08-26 20:06:09 +0100

    added multiple enemy tiers spawning in the spawner, boss is now implemented into the spawner too

--- Project Files/Epitaph/Assets/Scripts/Game Scripts/PlayScript.cs
+++ Project Files/Epitaph/Assets/Scripts/Game Scripts/PlayScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-[assembly: System.Reflection.AssemblyVersion("0.0.2.1")]
+[assembly: System.Reflection.AssemblyVersion("0.1.0.0")]
 
 public class PlayScript : MonoBehaviour 
 {
--- Project Files/Epitaph/Assets/Scripts/Game Scripts/spawnEmitter.cs
+++ Project Files/Epitaph/Assets/Scripts/Game Scripts/spawnEmitter.cs
@@ -2,51 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawnEmitter : MonoBehaviour {
+public class spawnEmitter : MonoBehaviour 
+{
 
-	int randomSelector = 0;
+	//calls the game controller script
+	public GameController controller;
 
-	//Array that holds enemy gameobject prefab references
-	int enemyArraySize;
-	int specialEnemyArraySize;
+	//bools
+	public bool canSpawnBoss = true;
+	public bool bossSpawned = false;
 
-	public GameObject[] basicWave1;
-	public GameObject[] basicWave2;
-	public GameObject[] basicWave3;
-	public GameObject[] basicWave4;
-	public GameObject[] basicWave5;
-
-	public GameObject [] enemy;
-	public GameObject [] specialEnemy;
-	int currentRandomEnemy;
-	int currentRandomSpecialEnemy;
-
-	//Array that holds pickup gameobject prefab references
-	int pickupArraySize;
-	public GameObject [] pickup;
-	int currentRandomPickup;
-
-	//Holds the location of all emitters in an array for easy referencing
-	int emitterArraySize;
-	int topEmitterArraySize;
-	public GameObject [] emitters;
-	public GameObject [] topEmitters;
-	public GameObject [] bottomEmitters;
-	GameObject currentEmitter;
-	GameObject secondCurrentEmitter;
-	GameObject specialCurrentEmitter;
-	int randomEmitterNumber;
-	int topBottomRandomEmitterNumber;
-
-	int randomEmitterSpawn;
-
+	//floats
+	public float bossCounter = 1;
 	public float secondsBetweenEnemies;
 	public float secondsBetweenSpecialEnemies;
 	public float secondsBetweenPickups = 1f;
 	public float secondsBeforeBegin = 4f;
 
+	//integers
+	int randomSelector = 0;
+	int specialEnemyArraySize;
+	int currentRandomEnemy;
+	int currentRandomSpecialEnemy;
+	int pickupArraySize;
+	int currentRandomPickup;
+	int emitterArraySize = 0;
+	int topEmitterArraySize;
+	int randomEmitterNumber;
+	int topBottomRandomEmitterNumber;
+	int randomEmitterSpawn;
+
+	//Array that holds enemy gameobject prefab references
+	public GameObject [] waves;//array for the waves of enemies.
+	public GameObject [] enemy;
+	public GameObject [] specialEnemy;
+	public GameObject [] pickup;
+	public GameObject boss1;
+	public GameObject currentBoss;
+
+	//Holds the location of all emitters in an array for easy referencing
+	public GameObject [] emitters;
+	public GameObject [] topEmitters;
+	public GameObject [] bottomEmitters;
+	GameObject currentEmitter = null;
+	GameObject secondCurrentEmitter;
+	GameObject specialCurrentEmitter;
+
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		GetArrayDetails ();
 		currentEmitter = emitters [1]; //needs to be assigned on start
 		secondCurrentEmitter = emitters [2]; //needs to be assigned on start
@@ -54,100 +59,152 @@ public class spawnEmitter : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		RandomGenerator ();
+		boss ();
 	}
 
 	//gets the details of array length at startup and then saves them to variables for use by the random generators
-	void GetArrayDetails () {
+	void GetArrayDetails () 
+	{
 		emitterArraySize = emitters.Length ;
 		topEmitterArraySize = topEmitters.Length;
 		pickupArraySize = pickup.Length;
-		enemyArraySize = enemy.Length;
 		specialEnemyArraySize = specialEnemy.Length;
-//		Debug.Log (emitterArraySize);
-//		Debug.Log (enemyArraySize);
-//		Debug.Log (pickupArraySize);
 	}
 
 	//generates random numbers to call items from the arrays
-	void RandomGenerator () {
+	void RandomGenerator () 
+	{//if statements to check the gamescore and spawn harder enemies
+		if (GameController.gameScore < 5000)
+		{
 		randomSelector = Random.Range (0, 5); //selects for waves!!!
+		}
+		else if (GameController.gameScore >= 10000 && GameController.gameScore < 15000)
+		{
+			randomSelector = Random.Range (0, 10); //selects for waves!!!
+		}	
+		else if (GameController.gameScore >= 15000)
+		{
+			randomSelector = Random.Range (0, 15); //selects for waves!!!
+		}
 
-		randomEmitterNumber = Random.Range (0, emitterArraySize);
+		//generates randoms	
 		topBottomRandomEmitterNumber = Random.Range (0, topEmitterArraySize);
 		currentRandomPickup =  Random.Range (0, pickupArraySize);
-		currentRandomEnemy = Random.Range (0, enemyArraySize);
 		currentRandomSpecialEnemy = Random.Range (0, specialEnemyArraySize);
-
 		randomEmitterSpawn = Random.Range (0, 2);
 
-		currentEmitter = emitters [randomEmitterNumber];
-		//secondCurrentEmitter = emitters [secondRandomEmitterNumber];
-
-
-		//these control whether to use th
-		if (randomEmitterSpawn == 0) {
+		//controls where the special enemies spawn
+		if (randomEmitterSpawn == 0) 
+		{
 			specialCurrentEmitter = topEmitters [topBottomRandomEmitterNumber];
 		}
-		if (randomEmitterSpawn == 1) {
+		if (randomEmitterSpawn == 1) 
+		{
 			specialCurrentEmitter = bottomEmitters [topBottomRandomEmitterNumber];
 		}
 	}
 
-	IEnumerator Initialization () {
+	IEnumerator Initialization () 
+	{
 		yield return new WaitForSeconds (secondsBeforeBegin);
 		StartCoroutine ("SpawnPickup");
-		//StartCoroutine ("SpawnEnemy");
-
 		StartCoroutine ("SpawnWaves");
-
 		StartCoroutine ("SpawnSpecialEnemy");
 	}
 
-	//Coroutine that handles spawning enemies
-	IEnumerator SpawnEnemy () {
-		while (true) {
-			GameObject multispawn = currentEmitter; //hold the spawn emitter number until enmy group is out
-			Instantiate (enemy [0], multispawn.transform.position , Quaternion.identity);
-			yield return new WaitForSeconds(secondsBetweenEnemies); //for testing multiple enemies
-//			GameObject multispawn = currentEmitter; //hold the spawn emitter number until enmy group is out
-//			Instantiate (enemy [0], multispawn.transform.position , Quaternion.identity);
-//			yield return new WaitForSeconds(secondsBetweenEnemies); //for testing multiple enemies
-		}
-	}
-
-
 	//rebuilt for waves!!!
-	IEnumerator SpawnWaves () {
-		while (true) {
-			if (randomSelector == 0) {
-				Instantiate (basicWave1 [0], emitters [4].transform.position, Quaternion.identity);
-				Instantiate (basicWave1 [1], emitters [4].transform.position, Quaternion.identity);
+	IEnumerator SpawnWaves () 
+	{
+		while (true) 
+		{
+			if (randomSelector == 0) 
+			{
+				Instantiate (waves [0], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [1], emitters [4].transform.position, Quaternion.identity);
+			} 
+			else if (randomSelector == 1) 
+			{
+				Instantiate (waves [2], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [3], emitters [4].transform.position, Quaternion.identity);
 			}
-			if (randomSelector == 1) {
-				Instantiate (basicWave2 [0], emitters [4].transform.position, Quaternion.identity);
-				Instantiate (basicWave2 [1], emitters [4].transform.position, Quaternion.identity);
+			else if (randomSelector == 2) 
+			{
+				Instantiate (waves [4], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [5], emitters [4].transform.position, Quaternion.identity);
 			}
-			if (randomSelector == 2) {
-				Instantiate (basicWave3 [0], emitters [4].transform.position, Quaternion.identity);
-				Instantiate (basicWave3 [1], emitters [4].transform.position, Quaternion.identity);
+			else if (randomSelector == 3) 
+			{
+				Instantiate (waves [6], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [7], emitters [4].transform.position, Quaternion.identity);
 			}
-			if (randomSelector == 3) {
-				Instantiate (basicWave4 [0], emitters [4].transform.position, Quaternion.identity);
-				Instantiate (basicWave4 [1], emitters [4].transform.position, Quaternion.identity);
+			else if (randomSelector == 4) 
+			{
+				Instantiate (waves [8], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [9], emitters [4].transform.position, Quaternion.identity);
 			}
-			if (randomSelector == 4) {
-				Instantiate (basicWave5 [0], emitters [4].transform.position, Quaternion.identity);
-				Instantiate (basicWave5 [1], emitters [4].transform.position, Quaternion.identity);
+			else if (randomSelector == 5) 
+			{
+				Instantiate (waves [10], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [11], emitters [4].transform.position, Quaternion.identity);
 			}
+			else if (randomSelector == 6) 
+			{
+				Instantiate (waves [12], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [13], emitters [4].transform.position, Quaternion.identity);
+			}
+			else if (randomSelector == 7) 
+			{
+				Instantiate (waves [14], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [15], emitters [4].transform.position, Quaternion.identity);
+			}
+			else if (randomSelector == 8) 
+			{
+				Instantiate (waves [16], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [17], emitters [4].transform.position, Quaternion.identity);
+			}
+			else if (randomSelector == 9) 
+			{
+				Instantiate (waves [18], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [19], emitters [4].transform.position, Quaternion.identity);
+			}
+			else if (randomSelector == 10) 
+			{
+				Instantiate (waves [20], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [21], emitters [4].transform.position, Quaternion.identity);
+			}
+			else if (randomSelector == 11) 
+			{
+				Instantiate (waves [22], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [23], emitters [4].transform.position, Quaternion.identity);
+			}
+			else if (randomSelector == 12) 
+			{
+				Instantiate (waves [24], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [25], emitters [4].transform.position, Quaternion.identity);
+			}
+			else if (randomSelector == 13) 
+			{
+				Instantiate (waves [26], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [27], emitters [4].transform.position, Quaternion.identity);
+			}
+			else if (randomSelector == 14) 
+			{
+				Instantiate (waves [28], emitters [4].transform.position, Quaternion.identity);
+				Instantiate (waves [29], emitters [4].transform.position, Quaternion.identity);
+			}
+				
 			yield return new WaitForSeconds (secondsBetweenEnemies);
 		}
 	}
 
 	//Coroutine that handles spawning enemies
-	IEnumerator SpawnSpecialEnemy () {
-		while (true) {
+	IEnumerator SpawnSpecialEnemy () 
+	{
+		while (true) 
+		{
 			yield return new WaitForSeconds (secondsBetweenSpecialEnemies);
 			Instantiate (specialEnemy [currentRandomSpecialEnemy], specialCurrentEmitter.transform.position , Quaternion.identity);
 			yield return new WaitForSeconds(secondsBetweenSpecialEnemies); //for testing multiple enemies
@@ -155,11 +212,32 @@ public class spawnEmitter : MonoBehaviour {
 	}
 	 
 	//Coroutine that handles pickup spawning
-	IEnumerator SpawnPickup () {
-		while (true) {
+	IEnumerator SpawnPickup () 
+	{
+		while (true) 
+		{
 			Instantiate (pickup [currentRandomPickup], secondCurrentEmitter.transform.position , Quaternion.identity);
 			yield return new WaitForSeconds(secondsBetweenPickups);
 		}
 	}
 
+	void boss ()
+	{
+		if (GameController.gameScore >= (bossCounter * 15000) && canSpawnBoss == true) 
+		{
+			StopCoroutine ("SpawnWaves");
+			StopCoroutine ("SpawnSpecialEnemy");
+			currentBoss = Instantiate (boss1, emitters [4].transform.position, Quaternion.identity) as GameObject;
+			canSpawnBoss = false;
+			bossSpawned = true;
+			bossCounter++;
+		}
+		if (currentBoss == null && bossSpawned == true) 
+		{
+			StartCoroutine ("SpawnWaves");
+			StartCoroutine ("SpawnSpecialEnemy");
+			bossSpawned = false;
+			canSpawnBoss = true;
+		}
+	}
 }
