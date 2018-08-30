18888669b23c532ff20a7b1f89882f6246a12404
Graithen <grey.jim@gmail.com>
2018-08-30 12:08:40 +0100

    Changes in order to get project ready for play service implementation
    Project now set up to export to play store.

--- Project Files/Epitaph/Assets/Scripts/Game Scripts/PlayScript.cs
+++ Project Files/Epitaph/Assets/Scripts/Game Scripts/PlayScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-[assembly: System.Reflection.AssemblyVersion("0.1.1.0")]
+[assembly: System.Reflection.AssemblyVersion("0.1.2.1")]
 
 public class PlayScript : MonoBehaviour 
 {
